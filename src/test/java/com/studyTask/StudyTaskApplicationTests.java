2970afc9dfe09044d6e4c2f0b8368215cb20ad3b
gwoni <gwoni52@gmail.com>
2020-07-18 22:50:49 +0900

    create back-end scaffold

---
+++ src/test/java/com/studyTask/StudyTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studyTask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
